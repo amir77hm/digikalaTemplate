63258a6ff56e43b73fa1e69f145f7a18d2a5b3f2
amir77hm <amir77hm@gmail.com>
2020-11-14 22:59:16 +0330

    first version

---
+++ js/backToTopBTN.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#backTop').click(function(){
+        $('html,body').animate(
+            {scrollTop:0},
+            "slow"
+        )
+        return false;
+    })
+})
